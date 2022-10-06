c4aa780316c569ee44c27ef98a06238cd497567f
HEIWAY\OJEDAA01 <aldo.ojedar@gmail.com>
2022-10-06 16:26:55 -0500

    AGREGAR DATOS CON INSERT INTO

--- tarea4_sql.sql
+++ tarea4_sql.sql
@@ -14,17 +14,69 @@ situation VARCHAR(10),
 channel  VARCHAR (10)
 );
 
+
+
+
+INSERT INTO infoClients (id_cliente, name_client,dr, gz, gec,condicion, postal_code, date_start, situation, channel )
+
+VALUES(0001,'cliente1','DR NOROESTE','GZ - BC','Titanium',51,2022,'2020-03-01','baja','Off Trade'),
+      (0002,'cliente2','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade'),
+      (0003,'cliente3','DR NOROESTE','GZ - BC','Titanium',51,2022,'2020-03-01','activo','Off Trade'),
+      (0004, 'cliente4','DR NOROESTE','GZ - BC','Titanium',51,2022,'2020-03-01','activo','Off Trade'),
+      (0005, 'cliente5','DR NOROESTE','GZ - BC','Titanium',51,2022,'2019-04-01','activo','Off Trade'),
+      (0006, 'cliente6','DR NOROESTE','GZ - BC','Oro',51,2022,'2020-03-01','activo','Off Trade'),
+      (0007,'cliente7','DR NOROESTE','GZ - BC','Titanium',52,2022,'2020-03-01','activo','Off Trade'),
+      (0008,'cliente8','DR NOROESTE','GZ - BC','Titanium',52,2022,'2020-03-01','activo','Off Trade'),
+      (0009,'cliente9','DR NOROESTE','GZ - BC','Titanium',52,2022,'2020-03-01','activo','Off Trade'),
+      (0010,'cliente10','DR NOROESTE','GZ - BC','Titanium',52,2022,'2018-03-01','activo','Off Trade'),
+      (0011,'cliente11','DR NOROESTE','GZ - BC','Titanium',52,2022,'2020-03-01','activo','Off Trade'),
+      (0012,'cliente12','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade'),
+      (0013,'cliente13','DR NOROESTE','GZ - BC','Titanium',51,2022,'2020-03-01','activo','Off Trade'),
+      (0014,'cliente14','DR NOROESTE','GZ - BC','Platino',51,2022,'2017-03-01','activo','Off Trade'),
+      (0015,'cliente15','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade'),
+      (0016,'cliente16','DR NOROESTE','GZ - BC','Oro',51,2022,'2020-03-01','activo','Off Trade'),
+      (0017,'cliente17','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade'),
+      (0018,'cliente18','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade'),
+      (0019,'cliente19','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade'),
+      (0020,'cliente20','DR NOROESTE','GZ - BC','Platino',52,2022,'2020-03-01','activo','Off Trade');
+
+
 CREATE TABLE dailySales (
 id_pedido INT AUTO_INCREMENT KEY,
 id_product  INT,
 id_cliente INT, 
 date_purchase DATE,
-volumne FLOAT,
+volume FLOAT,
 income FLOAT,
 revenue FLOAT,
 ayudas FLOAT,
 cajas INT
 );
+
+
+INSERT INTO dailySales(id_pedido, id_product, id_cliente, date_purchase, volume, income, ayudas, cajas)
+VALUES  (1  ,1  ,1 ,'2022-01-01' ,0.2 ,2300 ,10  ,1),
+        (2 ,2  ,1 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (3 ,3  ,1 ,'2022-01-01' ,0.3 ,2500 ,10  ,1),
+        (4 ,4  ,1 ,'2022-01-01' ,0.3 ,2600 ,10  ,1),
+        (5 ,5  ,1 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (6, 1 ,2 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (7 ,2 ,2 ,'2022-01-01' ,0.3 ,2900 ,10  ,1),
+        (8 ,3 ,2 ,'2022-01-01' ,0.3 ,2100 ,10  ,1),
+        (9 ,4 ,2 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (10 ,5 ,2 ,'2022-01-01' ,0.3 ,2000 ,10  ,1),
+        (11,6 ,2 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (12  ,1 ,3 ,'2022-01-01' ,0.3 ,2300 ,10 ,1),
+        (13,2 ,3 ,'2022-01-01' ,0.3 ,2800 ,10  ,1),
+         (14 ,3 ,2 ,'2022-01-01' ,0.3 ,2100 ,10  ,1),
+        (15 ,4 ,2 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (16 ,5 ,2 ,'2022-01-01' ,0.3 ,2000 ,10  ,1),
+        (17,6 ,2 ,'2022-01-01' ,0.3 ,2300 ,10  ,1),
+        (18  ,1 ,3 ,'2022-01-01' ,0.3 ,2300 ,10 ,1),
+        (19,2 ,3 ,'2022-01-01' ,0.3 ,2800 ,10  ,1),
+        (20 ,3 ,4 ,'2022-01-01' ,0.3 ,2300 ,10  ,1);
+
+
 
 CREATE TABLE coldCapacity (
 id_fridge INT,
@@ -33,6 +85,30 @@ id_cliente INT,
 date_in DATE,
 eq_status VARCHAR(15)
 );
+
+INSERT INTO coldCapacity(id_fridge, cold_capacity, id_cliente, date_in,eq_status)
+ VALUES (1,320,1,'2022-01-01','APEGO'),
+        (2,320,2,'2022-01-01','SUB EQUIPADO'),
+        (3,320,3,'2022-01-01','SOBRE EQUIPADO'),
+        (4,320,4,'2022-01-01','APEGO'),
+        (5,320,5,'2022-01-01','SUB EQUIPADO'),
+        (6,320,6,'2022-01-01','SOBRE EQUIPADO'),
+        (7,320,7,'2022-01-01','APEGO'),
+        (8,320,8,'2022-01-01','SUB EQUIPADO'),
+        (9,550,9,'2022-01-01','SOBRE EQUIPADO'),
+        (10,550,10,'2022-01-01','APEGO'),
+        (11,550,11,'2022-01-01','SUB EQUIPADO'),
+        (12,550,12,'2022-01-01','SOBRE EQUIPADO'),
+        (13,550,13,'2022-01-01','APEGO'),
+        (14,550,14,'2022-01-01','SUB EQUIPADO'),
+        (15,550,15,'2022-01-01','SOBRE EQUIPADO'),
+        (16,550,16,'2022-01-01','APEGO'),
+        (17,550,17,'2022-01-01','SUB EQUIPADO'),
+        (18,840,18,'2022-01-01','SOBRE EQUIPADO'),
+        (19,840,19,'2022-01-01','APEGO'),
+        (20,840,20,'2022-01-01','SUB EQUIPADO');
+
+
 
 CREATE TABLE catalogSKU (
  id_product  INT,
@@ -46,3 +122,6 @@ CREATE TABLE catalogSKU (
  pack_size VARCHAR(20),
  pack_type VARCHAR(20)
   );
+
+
+
